c948d190a0828afccf29ca10d3d8b933c20bc824
Nikita Oboldin <nikseljkee1@gmail.com>
2020-09-09 16:03:48 +0300

    Added unit tests for companies and genres

--- API.Tests/GamesControllerTests.cs
+++ API.Tests/GamesControllerTests.cs
@@ -18,11 +18,10 @@ namespace API.Tests
         [Test]
         public async Task GetAllGames_ShouldReturnAllGames()
         {
-            Pagination<GameToReturnDto> pagination = new Pagination<GameToReturnDto>();
             string requestUrl = "http://localhost:5000/api/games";
 
             var response = await _testClient.GetAsync(requestUrl);
-            pagination = await _testClient.GetJsonAsync<Pagination<GameToReturnDto>>(requestUrl);
+            var pagination = await _testClient.GetJsonAsync<Pagination<GameToReturnDto>>(requestUrl);
 
             Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
             Assert.IsNotNull(pagination);
@@ -32,16 +31,39 @@ namespace API.Tests
         [Test]
         public async Task GetGame_ShouldReturnGameById()
         {
-            GameToReturnDto game = new GameToReturnDto();
             int id = 1;
             string requestUrl = "http://localhost:5000/api/games/" + id;
 
             var response = await _testClient.GetAsync(requestUrl);
-            game = await _testClient.GetJsonAsync<GameToReturnDto>(requestUrl);
+            var game = await _testClient.GetJsonAsync<GameToReturnDto>(requestUrl);
 
             Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
             Assert.IsNotNull(game);
             Assert.AreEqual(id, game.Id);
+        }
+
+        [Test]
+        public async Task GetGameCompanies_ShouldReturnAllGameCompanies()
+        {
+            string requestUrl = "http://localhost:5000/api/games/companies";
+
+            var response = await _testClient.GetAsync(requestUrl);
+            var companies = await _testClient.GetJsonAsync<IReadOnlyList<Company>>(requestUrl);
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            Assert.IsNotNull(companies);
+        }
+
+        [Test]
+        public async Task GetGameGenres_ShouldReturnAllGameGenres()
+        {
+            string requestUrl = "http://localhost:5000/api/games/genres";
+
+            var response = await _testClient.GetAsync(requestUrl);
+            var companies = await _testClient.GetJsonAsync<IReadOnlyList<Genre>>(requestUrl);
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            Assert.IsNotNull(companies);
         }
     }
 }
