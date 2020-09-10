6d5bd21e9e5801883c48488d071a6cf8713bedda
Nikita Oboldin <nikseljkee1@gmail.com>
2020-09-10 15:40:07 +0300

    Added test method post for games controller

--- API.Tests/GamesControllerTests.cs
+++ API.Tests/GamesControllerTests.cs
@@ -65,5 +65,36 @@ namespace API.Tests
             Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
             Assert.IsNotNull(companies);
         }
+
+        [Test]
+        public async Task CreateGame_ShouldReturnCreatedGame()
+        {
+            string requestUrl = "http://localhost:5000/api/games";
+            string imageUrl = "http://localhost:5000";
+            GameForCreatingDto gameForCreating = new GameForCreatingDto
+            {
+                Title = "New Game",
+                Description = "New Game by Company",
+                Price = 1000,
+                PictureUrl = "images/games/new_game.png",
+                CompanyId = 1,
+                GenreId = 1
+            };
+
+            var companies = await _testClient.GetJsonAsync<List<Company>>(requestUrl + "/companies");
+            var genres = await _testClient.GetJsonAsync<List<Genre>>(requestUrl + "/genres");
+
+            var companyForCreatedGame = companies.Find(x => x.Id == gameForCreating.CompanyId);
+            var genreForCreatedGame = genres.Find(x => x.Id == gameForCreating.GenreId);
+
+            var createdGame = await _testClient.PostJsonAsync<GameToReturnDto>(requestUrl, gameForCreating);
+
+            Assert.AreEqual("New Game", createdGame.Title);
+            Assert.AreEqual("New Game by Company", createdGame.Description);
+            Assert.AreEqual(1000, createdGame.Price);
+            Assert.AreEqual(imageUrl + "/images/games/new_game.png", createdGame.PictureUrl);
+            Assert.AreEqual(companyForCreatedGame.Name, createdGame.Company);
+            Assert.AreEqual(genreForCreatedGame.Name, createdGame.Genre);
+        }
     }
 }
